035dab3c3f8272870c8f38599e0fd13faf5a36cc
BanditDid <104263831+BanditDid@users.noreply.github.com>
2025-12-09 20:23:47 +0700

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -6,7 +6,7 @@ import { MockService } from './mockService';
 // อ่านค่า Config จาก Environment Variable
 // ถ้าตั้งค่า NEXT_PUBLIC_USE_MOCK=true จะใช้ MockService
 //const USE_MOCK = process.env.NEXT_PUBLIC_USE_MOCK === 'true';
-const USE_MOCK ='true';
+const USE_MOCK ='false';
 
 console.log(`[System] Running in ${USE_MOCK ? 'MOCK' : 'PRODUCTION (Google)'} mode`);
 
